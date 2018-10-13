5a637d29d1c09b87f91ea1912b1a6968540ffdb2
Oluwasegun Adeleye <i.segunadeleye@gmail.com>
2018-10-13 22:44:05 +0100

    Parse payload sent to the server.

--- rest-api/app/index.js
+++ rest-api/app/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 const server = http.createServer((req, res) => {
   // Get the URL and parse it
@@ -18,7 +19,24 @@ const server = http.createServer((req, res) => {
   // Get the headers as an object
   const headers = req.headers;
 
-  res.end(`Hello World from /${ trimmedPath }\n`);
+  // Required to decode the streams of data
+  // from the HTTP request into string
+  const decoder = new StringDecoder('utf-8');
+  const buffer = '';
+
+  // A "data" event is emmited each time a stream is received
+  req.on('data', data => {
+    buffer += decoder.write(data)
+  });
+
+  // After all streams have been received, an "end" event is emmited
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    console.log('Request received with this payload: ', buffer);
+    res.end(`Hello World from /${ trimmedPath }\n`);
+  });
+
 });
 
 server.listen(3000, () => {
